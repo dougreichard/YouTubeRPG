3a06030c49e5520587c4e5a9ce6933f18890609a
dougreichard <doug.reichard+github@gmail.com>
2024-05-30 23:33:06 -0400

    added combat system

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,27 +1,65 @@
 extends CharacterBody2D
 
-var speed = 25
+var speed = 50
 var player_chase = false
 var player = null
+var health = 100
+var player_in_attack_zone = false
+var can_take_damage = true
+var played_dead = false
 
 func _physics_process(delta):
+	if health<=0:
+		if not played_dead:
+			played_dead = true
+			$AnimatedSprite2D.play("death")
+		return
+		
+	deal_with_damage()
 	if player_chase:
 		position += (player.position - position)/speed
-
 		$AnimatedSprite2D.flip_h = player.position.x < position.x
 		
 	
 
 
 func _on_detection_body_entered(body):
-	player = body
-	player_chase = true
-	$AnimatedSprite2D.play("move_side")
+	if health>0:
+		player = body
+		player_chase = true
+		$AnimatedSprite2D.play("move_side")
 	
 
 
 func _on_detection_body_exited(body):
+	
 	player = null
 	player_chase = false
-	$AnimatedSprite2D.play("move_idle")
+	if health>0:
+		$AnimatedSprite2D.play("move_idle")
 	
+func enemy():
+	pass
+
+
+func _on_enemy_hit_box_body_entered(body):
+	if body.has_method("player"):
+		player_in_attack_zone = true
+
+
+func _on_enemy_hit_box_body_exited(body):
+	player_in_attack_zone = false
+
+func deal_with_damage():
+	if can_take_damage and player_in_attack_zone and Global.player_current_attack and health > 0:
+		health -= 20
+		print("health {0}".format([health]))
+		can_take_damage = false
+		$take_damage_cooldown.start()
+		
+		
+
+
+func _on_take_damage_cooldown_timeout():
+	can_take_damage = true
+	$take_damage_cooldown.stop()
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,27 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-var anim_dir = "idle"
+var anim_dir = "front"
 
+var enemy_in_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var alive = true
+var player_died = false
+var attack_in = false
 
 func _physics_process(_delta):
+	player_movement(_delta)
+	enemy_attack()
+	play_attack_animation()
+	
+	
+	if health <= 0 and alive:
+		alive = false
+		$player_anim.play("die")
+	
+
+func player_movement(_delta):
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
 		velocity.y = 0	
@@ -30,7 +47,9 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func play_animation(movement):
-	var anim = $AnimatedSprite2D
+	var anim = $player_anim
+	if attack_in:
+		return
 	if movement == anim_dir:
 		return
 	if movement == "right":
@@ -53,4 +72,59 @@ func play_animation(movement):
 		anim.play("idle_back")	
 	else:
 		anim.play("idle_front")	
-	anim_dir = movement
+	if movement != "idle":
+		anim_dir = movement	
+	
+	
+func play_attack_animation():
+	var anim = $player_anim
+		
+	if Input.is_action_just_pressed("attack"):
+		Global.player_current_attack = true
+		attack_in = true
+		$deal_attack.start()
+		#print("Swing "+anim_dir)
+		if anim_dir == "right":
+			anim.flip_h = false
+			anim.play("attack_side")
+		elif anim_dir == "left":
+			anim.flip_h = true
+			anim.play("attack_side")
+		elif anim_dir == "up":
+			anim.flip_h = false
+			anim.play("attack_back")	
+		elif anim_dir == "down":
+			anim.flip_h = false
+			anim.play("attack_front")	
+		
+
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		if body.health >0:
+			enemy_in_range = true
+
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_in_range =false
+
+func player():
+	pass
+
+func enemy_attack():
+	if enemy_in_range and enemy_attack_cooldown:
+		health -= 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print("health {0}".format([health]) )
+		
+
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
+
+
+func _on_deal_attack_timeout():
+	$deal_attack.stop()
+	Global.player_current_attack = false
+	attack_in = false
